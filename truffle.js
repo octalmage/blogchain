6c4669232782904fcd3fad888518b02e3c1e83ff
Jason Stallings <jason@stallin.gs>
2017-09-12 16:13:34 -0500

    Add staging address.

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    staging: {
+      host: "159.203.148.209",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   }
 };
