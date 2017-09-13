60d9e9f3ac3f662061433063f8af7ed96fa3d8fa
Jay Hill <jay.hill@wpengine.com>
2017-09-12 19:04:49 -0500

    Changed BrowserRouter to HashRouter to allow for deployment to ipfs.io

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import App from './App'
 import SinglePost from './SinglePost'
-import Author from './Author.tsx'
+import Author from './Author'
 
 const Router = ()  => (
-  <BrowserRouter>
+  <HashRouter>
     <div>
         <Route exact path="/" component={App} />
         <Route path="/post/:post_id" component={SinglePost} />
         <Route path="/author/:author_id" component={Author} />
     </div>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 export default Router;
